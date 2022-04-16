76f90e2e6d33e37da973a45c2c9bd174437f2b82
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-16 18:09:00 +0600

    Navbar working on progress for mobile device

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsPerson } from 'react-icons/bs';
 import { BiSearch } from 'react-icons/bi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaPinterest,
+  FaYoutube,
+} from 'react-icons/fa';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="flex justify-between items-center h-20 px-4">
       <div>
         <h1>TRAVEL.</h1>
       </div>
-      <ul className="flex">
+      <ul className="hidden md:flex">
         <li>HOME</li>
         <li>DESTINATION</li>
         <li>TRAVEL</li>
         <li>VIEW</li>
         <li>BOOK</li>
       </ul>
-      <div className="flex">
-        <BiSearch />
-        <BsPerson />
+      <div className="hidden md:flex">
+        <BiSearch size={20} />
+        <BsPerson size={20} />
+      </div>
+
+      {/* Hamburger */}
+      <div onClick={handleNav} className="md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <HiOutlineMenuAlt4 size={20} />}
+      </div>
+
+      {/* Mobile Menu Dropdown */}
+      <div
+        className={
+          nav
+            ? 'absolute left-[0%] top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col'
+            : 'absolute left-[-100%]'
+        }
+      >
+        <ul>
+          <h1>TRAVEL.</h1>
+          <li className="border-b">HOME</li>
+          <li className="border-b">DESTINATION</li>
+          <li className="border-b">TRAVEL</li>
+          <li className="border-b">VIEW</li>
+          <li className="border-b">BOOK</li>
+        </ul>
+        <div className="flex flex-col">
+          <button className="my-6">Search</button>
+          <button>Account</button>
+        </div>
+        <div className="flex justify-between my-6">
+          <FaFacebook className="icon" />
+          <FaTwitter className="icon" />
+          <FaPinterest className="icon" />
+          <FaYoutube className="icon" />
+          <FaInstagram className="icon" />
+        </div>
       </div>
     </div>
   );
